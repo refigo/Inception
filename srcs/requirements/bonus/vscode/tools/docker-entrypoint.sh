a61f04fd9770f2c3ca59684de226e96ccfb24502
refigo <has0521@daum.net>
2023-05-24 21:55:31 +0900

    REFACTOR: Modify vscode port number

--- srcs/requirements/bonus/vscode/tools/docker-entrypoint.sh
+++ srcs/requirements/bonus/vscode/tools/docker-entrypoint.sh
@@ -5,4 +5,4 @@ wget https://github.com/coder/code-server/releases/download/v4.8.3/code-server-4
 tar xf code-server-4.8.3-linux-amd64.tar.gz -C /app/code-server --strip-components=1
 ln -s /app/code-server/bin/code-server /usr/bin/code-server
 
-exec code-server --auth=password --bind-addr=0.0.0.0:8443 --user-data-dir=/var/www/html --cert=/etc/ssl/certs/vscode-selfsigned.crt --cert-key=/etc/ssl/private/vscode-selfsigned.key
+exec code-server --auth=password --bind-addr=0.0.0.0:8080 --user-data-dir=/var/www/html --cert=/etc/ssl/certs/vscode-selfsigned.crt --cert-key=/etc/ssl/private/vscode-selfsigned.key
