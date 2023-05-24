6ca5c9e0095f1bb9ba6cd0c726527bd8d470c4a7
refigo <has0521@daum.net>
2023-05-24 19:17:40 +0900

    FEAT: Set adminer usable

--- srcs/requirements/bonus/adminer/tools/docker-entrypoint.sh
+++ srcs/requirements/bonus/adminer/tools/docker-entrypoint.sh
@@ -1,12 +1,6 @@
 # !/bin/sh
 
-echo "listen = 8000" >> /etc/php/7.4/fpm/pool.d/www.conf
-mkdir -p /var/www/wordpress/adminer
-curl -s -L https://github.com/vrana/adminer/releases/download/v4.8.1/adminer-4.8.1-mysql-en.php --output /var/www/wordpress/adminer/index.php
+mkdir -p /var/www/html/adminer
+curl -s -L https://github.com/vrana/adminer/releases/download/v4.8.1/adminer-4.8.1-mysql-en.php --output /var/www/html/adminer/index.php
 
-# after run dumb init
-php-fpm7.4 --nodaemonize 
-
-# sed -i "/listen = /c\listen = 0.0.0.0:8080" /etc/php8/php-fpm.d/www.conf
-# sed -i "/user = nobody/c\user = www-data" /etc/php8/php-fpm.d/www.conf
-# sed -i "/group = nobody/c\group = www-data" /etc/php8/php-fpm.d/www.conf
+exec php-fpm7.3 --nodaemonize
